dc592270573ba46d03130623e6595f8a2d711e09
Rebecka Kinn <rebeckaki@getacademy.no>
2024-11-01 14:06:19 +0100

    added css to homepage

--- HomePage/homePageView.js
+++ HomePage/homePageView.js
@@ -2,11 +2,11 @@ function drawHomePageView(){
     app.innerHTML = /*HTML*/`
     ${header()}
     <section class="full-size flex-column center align-self-center">
-        <div class="flex-row">
-            <h1>Velkommen</h1>
+        <div class="top-section-grid">
+            <h1 class="freehand-font title">Velkommen</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare venenatis gravida. Nunc non feugiat dui. Quisque et mauris vitae tellus mollis gravida vel nec nunc. Fusce vitae dolor eu nibh luctus consectetur. Etiam ornare ultrices est, ut placerat augue ultrices eget. Nam tempus sem ligula, in lacinia metus ultricies lobortis.</p>
         </div>
-        <div>${drawTopActivities()}</div>
+        <div class="activity-showcase-horizontal">${drawTopActivities()}</div>
         <button onclick="showAllActivities()">Se alle aktiviteter</button>
     </section>
     `;
@@ -18,7 +18,7 @@ function drawTopActivities() {
         html += /*HTML*/`
             <div onclick="#">
                 <img src="${activity.activityImg}">
-                <h3>${activity.activityName}</h3>
+                <h2>${activity.activityName}</h2>
             </div>
         `;
     }
