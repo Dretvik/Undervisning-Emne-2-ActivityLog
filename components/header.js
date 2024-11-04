d64fcb8bb6ebd295d7d73c8883486467cda2e512
Rebecka Kinn <rebeckaki@getacademy.no>
2024-11-04 11:00:17 +0100

    added a menu!

--- components/header.js
+++ components/header.js
@@ -1,11 +1,20 @@
 
+
 function header(){
     return /*HTML*/`
     <header class="header-container flex-row">
         <div class="logo">
             <img src="assets/logo/activity_logg_logo.png" alt="activity_logg_logo">
         </div>
-
+        <span class="menu">
+        <input id="burgerMenu" type="checkbox" />
+            <label for="burgerMenu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+        ${menu()}
+        </span>
         
     </header>
     `;
--- components/menu.js
+++ components/menu.js
@@ -1,9 +1,66 @@
 
+function createMenuComponents(){
+    const menuItems= [
+        {
+            name: "Hjem",
+            path: "homePage"
+        },
+        {
+            name: "Profil",
+            path: null
+        },
+        {
+            name: "Mine aktiviteter",
+            path: "activityPage"
+        },
+        {
+            name: "Mine omtaler",
+            path: null
+        },
+        {
+            name: "Legg til aktivitet",
+            path: "addActivityPage"
+        },
+        {
+            name: "Logg ut",
+            path: "loginPage"
+        },
+    ];
+
+    let html = "";
+    menuItems.forEach((element) => {
+        console.log(element.path)
+        if(model.app.currentPage === element.path){
+            html += /*HTML*/
+        `
+        <li class="bold color-main-blue font-size-larger" onclick="menuElementChangePage('${element.path}')">${element.name}</li>
+        `
+        }
+        else if(model.app.currentPage === null){
+            html += /*HTML*/
+        `
+        <li class="color-gray" onclick="menuElementChangePage('${element.path}')">${element.name}</li>
+        `
+        }
+        else{
+            html += /*HTML*/
+            `
+            <li class="color-main-blue" onclick="menuElementChangePage('${element.path}')">${element.name}</li>
+            `
+        }
+    });
+    return html;
+}
+
+function menuElementChangePage(newPage){
+    model.app.currentPage = newPage;
+    updateView();
+}
 
 function menu() {
     return /*HTML*/`
-        <menu>
-            <li>test</li>
+        <menu class="remove-bullet flex-column gap-20 menuContainer">
+            ${createMenuComponents()}
         </menu>
     `;
 }
